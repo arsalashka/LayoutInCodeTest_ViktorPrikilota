a17faa890924e8ba19d9c608f57f4af244f09761
Arsalan Balzhinimaev <arsalashka@gmail.com>
2024-04-26 22:52:42 -0500

    Initial Commit

---
+++ LayoutInCodeTest_ViktorPrikilota/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LayoutInCodeTest_ViktorPrikilota
+//
+//  Created by Arsalan on 26.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
