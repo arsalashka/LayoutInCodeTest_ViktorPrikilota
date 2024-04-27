bdca4bd2f4ae5b7d810922b6967b5677f97b1e8f
Arsalan Balzhinimaev <arsalashka@gmail.com>
2024-04-27 00:59:01 -0500

    add setupSignupButton

--- LayoutInCodeTest_ViktorPrikilota/LoginViewController.swift
+++ LayoutInCodeTest_ViktorPrikilota/LoginViewController.swift
@@ -6,14 +6,70 @@
 //
 
 import UIKit
+import SnapKit
 
 class LoginViewController: UIViewController {
+    private let containerView = UIView()
+    private let loginLabel = UILabel()
+    private let signupButton = UIButton()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-//        view.backgroundColor = .gray
+        
+        view.backgroundColor = .white
+        
+        setupContainerView()
+        setupLoginLabel()
+        setupSignupButton()
     }
-
-
+    
+    private func setupContainerView() {
+        view.addSubview(containerView)
+        
+        containerView.backgroundColor = .lightGray
+        
+        containerView.snp.makeConstraints { make in
+            make.leading.equalToSuperview().inset(45)
+            make.trailing.equalToSuperview().inset(45)
+            make.top.equalToSuperview().inset(238)
+            make.bottom.equalToSuperview().inset(269)
+        }
+    }
+    
+    private func setupLoginLabel() {
+        view.addSubview(loginLabel)
+        
+        loginLabel.font = UIFont.systemFont(ofSize: 30, weight: .semibold)
+        loginLabel.text = "LOGIN /"
+        
+        loginLabel.snp.makeConstraints { make in
+            make.top.equalTo(containerView.snp.top)
+            make.leading.equalTo(containerView.snp.leading)
+        }
+    }
+    
+    private func setupSignupButton() {
+        view.addSubview(signupButton)
+        
+//        signupButton.backgroundColor = .blue
+        
+        signupButton.titleLabel?.font = UIFont.systemFont(ofSize: 20, weight: .regular)
+        signupButton.setTitle("Sign Up", for: .normal)
+        signupButton.setTitleColor(.gray, for: .normal)
+        
+     
+        signupButton.snp.makeConstraints { make in
+            make.leading.equalTo(loginLabel.snp.trailing).offset(10)
+            make.bottomMargin.equalTo(loginLabel.snp.bottom).inset(4)
+        }
+        
+        signupButton.addAction(UIAction { [weak self] _ in
+            let signupVC = UIViewController()
+            
+            signupVC.view.backgroundColor = .purple
+            signupVC.modalPresentationStyle = .fullScreen
+            
+            self?.present(signupVC, animated: true)
+        }, for: .touchUpInside)
+    }
 }
---
+++ LayoutInCodeTest_ViktorPrikilota/SignupViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SignupViewController.swift
+//  LayoutInCodeTest_ViktorPrikilota
+//
+//  Created by Arsalan on 27.04.2024.
+//
+
+import UIKit
+
+class SignupViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+ 
+
+}
